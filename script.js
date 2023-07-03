8906ff33460a2a2d0b6a6377d1d57bff8290b340
Marco Favero <59004866+marcofavero3@users.noreply.github.com>
2023-07-03 12:08:23 -0300

    commit init

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".multiple-text", {
+    strings: ["Full Stack Developer","Software Engineer","System Analyst"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
